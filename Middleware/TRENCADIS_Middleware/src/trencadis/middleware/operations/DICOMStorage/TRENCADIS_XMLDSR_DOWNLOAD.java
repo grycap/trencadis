0f846d67ae319c81e2aeb5e0efacc9db231e3747
Lorena Calabuig <lorenacalabuig@gmail.com>
2015-04-22 13:28:30 +0200

    Changed download operations of XMLDSR files

--- Middleware/TRENCADIS_Middleware/src/trencadis/middleware/operations/DICOMStorage/TRENCADIS_XMLDSR_DOWNLOAD.java
+++ Middleware/TRENCADIS_Middleware/src/trencadis/middleware/operations/DICOMStorage/TRENCADIS_XMLDSR_DOWNLOAD.java
@@ -1,10 +1,13 @@
 package trencadis.middleware.operations.DICOMStorage;
 
 import java.io.File;
+import java.util.Iterator;
+import java.util.Vector;
 
 import org.apache.commons.io.FileUtils;
 
-import trencadis.infrastructure.services.DICOMStorage.impl.wrapper.xmlOutputDownloadReport.XmlWrapper;
+import trencadis.infrastructure.services.DICOMStorage.impl.wrapper.xmlOutputDownloadAllReportsByOntology.XmlWrapper;
+import trencadis.infrastructure.services.stubs.DICOMStorage.DICOMStoragePortType;
 import trencadis.middleware.files.TRENCADIS_XML_DICOM_SR_FILE;
 import trencadis.middleware.login.TRENCADIS_SESSION;
 
@@ -14,27 +17,49 @@ import trencadis.middleware.login.TRENCADIS_SESSION;
  */
 public class TRENCADIS_XMLDSR_DOWNLOAD extends
 		TRENCADIS_OPERATION_DICOM_STORAGE_SERVICE {
-
-	private String _IDTRENCADISReport;
-	private String _IDOntology;
-
+	
+	private String idOntology = null;
+	private String idTrencadisReport = null;
+	
 	/**
 	 * This constructor is used when we have the hospital identifier and we need
 	 * to ask the IIS for the Storage DICOM URI.
 	 * 
 	 * @param session Session used by this operation
-	 * @param id_center Identifier of the hospital replica
-	 * @param reportType The report type of the DICOM-SR to download
-	 * @param reportID The report identifier of the DICOM-SR to download
 	 * @throws Exception
 	 */
-	public TRENCADIS_XMLDSR_DOWNLOAD(TRENCADIS_SESSION session,
-			TRENCADIS_XML_DICOM_SR_FILE xmlDsr, int id_center)
+	public TRENCADIS_XMLDSR_DOWNLOAD(TRENCADIS_SESSION session)throws Exception {
+		super(session);
+	}
+	
+	/**
+	 * This constructor is used when we have the hospital identifier and we need
+	 * to ask the IIS for the Storage DICOM URI.
+	 * 
+	 * @param session Session used by this operation
+	 * @param idCenter Identifier of the hospital replica
+	 * @throws Exception
+	 */
+	public TRENCADIS_XMLDSR_DOWNLOAD(TRENCADIS_SESSION session, int idCenter)
 			throws Exception {
-		super(session, id_center, xmlDsr.getIDOntology());
-
-		_IDOntology = xmlDsr.getIDOntology();
-		_IDTRENCADISReport = xmlDsr.getIDTRENCADISReport();
+		super(session, idCenter);
+	}
+	
+	/**
+	 * This constructor is used when we have the hospital identifier and we need
+	 * to ask the IIS for the Storage DICOM URI.
+	 * 
+	 * @param session Session used by this operation
+	 * @param idCenter Identifier of the hospital replica
+	 * @param xmlDsr The DICOM-SR to download
+	 * @throws Exception
+	 */
+	public TRENCADIS_XMLDSR_DOWNLOAD(TRENCADIS_SESSION session, int idCenter,
+			TRENCADIS_XML_DICOM_SR_FILE xmlDsr)
+			throws Exception {
+		super(session, idCenter, xmlDsr.getIDOntology());
+		this.idOntology = xmlDsr.getIDOntology();
+		this.idTrencadisReport = xmlDsr.getIDTRENCADISReport();
 	}
 
 	/**
@@ -42,43 +67,164 @@ public class TRENCADIS_XMLDSR_DOWNLOAD extends
 	 * to ask the IIS for the Storage DICOM URI.
 	 * 
 	 * @param session Session used by this operation
-	 * @param id_hospital Identifier of the hospital replica
-	 * @param reportType The report type of the DICOM-SR to download
-	 * @param reportID The report identifier of the DICOM-SR to download
+	 * @param idCenter Identifier of the hospital replica
+	 * @param idOntology The report type of the DICOM-SR to download
+	 * @param idTrencadisReport The report identifier of the DICOM-SR to download
 	 * @throws Exception
 	 */
-	public TRENCADIS_XMLDSR_DOWNLOAD(TRENCADIS_SESSION session,
-			String IDOntology, String IDTRENCADISReport, int idCenter)
+	public TRENCADIS_XMLDSR_DOWNLOAD(TRENCADIS_SESSION session, int idCenter,
+			String idOntology, String idTrencadisReport)
 			throws Exception {
-		super(session, idCenter, IDOntology);
-
-		_IDTRENCADISReport = IDTRENCADISReport;
-		_IDOntology = IDOntology;
+		super(session, idCenter, idOntology);
+		this.idOntology = idOntology;
+		this.idTrencadisReport = idTrencadisReport;
+	}
+	
+	/**
+	 * This constructor is used when we have the hospital identifier and we need
+	 * to ask the IIS for the Storage DICOM URI.
+	 * 
+	 * @param session Session used by this operation
+	 * @param idOntology The report type of the DICOM-SR to download
+	 * @throws Exception
+	 */
+	public TRENCADIS_XMLDSR_DOWNLOAD(TRENCADIS_SESSION session, String idOntology)
+			throws Exception {
+		super(session, idOntology);
+		this.idOntology = idOntology;
+	}
+	
+	/**
+	 * This constructor is used when we have the hospital identifier and we need
+	 * to ask the IIS for the Storage DICOM URI.
+	 * 
+	 * @param session Session used by this operation
+	 * @param idCenter Identifier of the hospital replica
+	 * @param idOntology The report type of the DICOM-SR to download
+	 * @throws Exception
+	 */
+	public TRENCADIS_XMLDSR_DOWNLOAD(TRENCADIS_SESSION session, int idCenter, String idOntology)
+			throws Exception {
+		super(session, idCenter, idOntology);
+		this.idOntology = idOntology;
 	}
 
 	/**
-	 * This method starts the download, also returns the downloaded file.
-	 * @return The downloaded file into a
+	 * This method starts the download, also returns the downloaded files in a Object
+	 * 
+	 * Each file is associated with the corresponding
+	 * {@link trencadis.middleware.operations.TRENCADIS_XML_DICOM_SR_FILE
+	 * TRENCADIS_XML_DICOM_SR_FILE}
+	 * 
+	 * @return Object The Object can be a {@link trencadis.middleware.files.TRENCADIS_XML_DICOM_SR_FILE
+	 *         TRENCADIS_XML_DICOM_SR_FILE} or a
 	 *         {@link trencadis.middleware.files.TRENCADIS_XML_DICOM_SR_FILE
-	 *         TRENCADIS_XML_DICOM_SR_FILE} object
+	 *         TRENCADIS_XML_DICOM_SR_FILE} vector
 	 * @throws Exception
 	 */
-	public TRENCADIS_XML_DICOM_SR_FILE execute() throws Exception {
-
+	public Object execute() throws Exception {	
+		
+		if (idOntology != null && idTrencadisReport != null)
+			return downloadReport();
+		else {
+			Vector<TRENCADIS_XML_DICOM_SR_FILE> retval = null;
+			if (DICOMStorages == null && idOntology != null) {
+				retval = downloadAllReportsByOntology(DICOMStorage);
+			} else if (DICOMStorages == null && idOntology == null) {
+				retval = downloadAllReports(DICOMStorage);
+			} else if (DICOMStorages != null && idOntology != null) {
+				retval = new Vector<TRENCADIS_XML_DICOM_SR_FILE>();
+				for (DICOMStoragePortType dicomStorage : DICOMStorages)
+					retval.addAll(downloadAllReportsByOntology(dicomStorage));
+			} else if (DICOMStorages != null && idOntology == null) {
+				retval = new Vector<TRENCADIS_XML_DICOM_SR_FILE>();
+				for (DICOMStoragePortType dicomStorage : DICOMStorages)
+					retval.addAll(downloadAllReports(dicomStorage));
+			} else {
+				throw new Exception("The parameters introduced are not invalid.");
+			}
+			return retval;	
+		}		
+		
+	}
+	
+	private TRENCADIS_XML_DICOM_SR_FILE downloadReport() throws Exception{
 		String xmlInputDownloadReport = "<?xml version=\"1.0\" encoding=\"UTF-8\"?>"
 				+ "<INPUT>\n"
 				+ "<CERTIFICATE>"
 				+ _session.getX509VOMSCredential()
 				+ "</CERTIFICATE>\n"
 				+ "<IDONTOLOGY>"
-				+ _IDOntology
+				+ idOntology
 				+ "</IDONTOLOGY>"
 				+ "<IDTRENCADISREPORT>"
-				+ _IDTRENCADISReport
+				+ idTrencadisReport
 				+ "</IDTRENCADISREPORT>\n" + "</INPUT>";
 
 		String xmlOutputDownloadReport = DICOMStorage.xmlDownloadReport(xmlInputDownloadReport);
 
+		trencadis.infrastructure.services.DICOMStorage.impl.wrapper.xmlOutputDownloadReport.XmlWrapper downloadReport_wrapper =
+				new trencadis.infrastructure.services.DICOMStorage.impl.wrapper.xmlOutputDownloadReport.XmlWrapper(xmlOutputDownloadReport, false);
+		downloadReport_wrapper.wrap();
+		FileUtils.writeStringToFile(new File("/home/locamo/example.txt"), xmlOutputDownloadReport);
+		if (!downloadReport_wrapper.get_OUTPUT().get_STATUS().equals("0")) {
+			throw new Exception("Can not download report: "
+					+ downloadReport_wrapper.get_OUTPUT().get_DESCRIPTION());
+		} else {
+			String DICOM_SR = downloadReport_wrapper.get_OUTPUT().get_DICOM_SR().get_xml_DICOM_SR();
+			TRENCADIS_XML_DICOM_SR_FILE dicom_sr = new TRENCADIS_XML_DICOM_SR_FILE(_session, DICOM_SR);
+			return dicom_sr;
+		}
+	}
+	
+	private Vector<TRENCADIS_XML_DICOM_SR_FILE> downloadAllReports(DICOMStoragePortType DICOMStorage) throws Exception {
+		Vector<TRENCADIS_XML_DICOM_SR_FILE> v_reports = null;
+		String xmlInputDownloadReport = "<?xml version=\"1.0\" encoding=\"UTF-8\"?>"
+				+ "<INPUT>\n"
+				+ "<CERTIFICATE>"
+				+ _session.getX509VOMSCredential()
+				+ "</CERTIFICATE>\n"
+				+ "</INPUT>";	
+
+		String xmlOutputDownloadReport = DICOMStorage.xmlDownloadAllReports(xmlInputDownloadReport);	
+		
+		trencadis.infrastructure.services.DICOMStorage.impl.wrapper.xmlOutputDownloadAllReports.XmlWrapper downloadReport_wrapper = 
+				new trencadis.infrastructure.services.DICOMStorage.impl.wrapper.xmlOutputDownloadAllReports.XmlWrapper(xmlOutputDownloadReport, false);
+		downloadReport_wrapper.wrap();
+		
+		if (!downloadReport_wrapper.get_OUTPUT().get_STATUS().equals("0")) {
+			throw new Exception("Can not download report: "
+					+ downloadReport_wrapper.get_OUTPUT().get_DESCRIPTION());
+		} else {
+			
+			Iterator it_reports = downloadReport_wrapper.get_OUTPUT().get_DICOM_REPORTS().getAll_DICOM_SR();
+			v_reports = new Vector<TRENCADIS_XML_DICOM_SR_FILE>();
+			while (it_reports.hasNext()){
+				trencadis.infrastructure.services.DICOMStorage.impl.wrapper.xmlOutputDownloadAllReports.DICOM_SR obj =
+						(trencadis.infrastructure.services.DICOMStorage.impl.wrapper.xmlOutputDownloadAllReports.DICOM_SR)it_reports.next();
+				String dicom_sr = obj.getValue();
+				TRENCADIS_XML_DICOM_SR_FILE xmldsr = new TRENCADIS_XML_DICOM_SR_FILE(_session, dicom_sr);
+				v_reports.add(xmldsr);
+			}			
+			return v_reports;
+
+		}
+	}
+	
+	private Vector<TRENCADIS_XML_DICOM_SR_FILE> downloadAllReportsByOntology(DICOMStoragePortType DICOMStorage) throws Exception {
+		Vector<TRENCADIS_XML_DICOM_SR_FILE> v_reports = null;
+		String xmlInputDownloadReport = "<?xml version=\"1.0\" encoding=\"UTF-8\"?>"
+				+ "<INPUT>\n"
+				+ "<CERTIFICATE>"
+				+ _session.getX509VOMSCredential()
+				+ "</CERTIFICATE>\n"
+				+ "<IDONTOLOGY>"
+				+ idOntology
+				+ "</IDONTOLOGY>"
+				+ "</INPUT>";	
+
+		String xmlOutputDownloadReport = DICOMStorage.xmlDownloadAllReportsByOntology(xmlInputDownloadReport);	
+		
 		XmlWrapper downloadReport_wrapper = new XmlWrapper(xmlOutputDownloadReport, false);
 		downloadReport_wrapper.wrap();
 		
@@ -86,13 +232,17 @@ public class TRENCADIS_XMLDSR_DOWNLOAD extends
 			throw new Exception("Can not download report: "
 					+ downloadReport_wrapper.get_OUTPUT().get_DESCRIPTION());
 		} else {
-
-			String DICOM_SR = downloadReport_wrapper.get_OUTPUT().get_DICOM_SR().get_xml_DICOM_SR();
-			TRENCADIS_XML_DICOM_SR_FILE dicom_sr = new TRENCADIS_XML_DICOM_SR_FILE(_session, DICOM_SR);
-			return dicom_sr;
-
+			Iterator it_reports = downloadReport_wrapper.get_OUTPUT().get_DICOM_REPORTS().getAll_DICOM_SR();
+			v_reports = new Vector<TRENCADIS_XML_DICOM_SR_FILE>();
+			while (it_reports.hasNext()){
+				trencadis.infrastructure.services.DICOMStorage.impl.wrapper.xmlOutputDownloadAllReportsByOntology.DICOM_SR obj =
+						(trencadis.infrastructure.services.DICOMStorage.impl.wrapper.xmlOutputDownloadAllReportsByOntology.DICOM_SR)it_reports.next();
+				String dicom_sr = obj.getValue();
+				TRENCADIS_XML_DICOM_SR_FILE xmldsr = new TRENCADIS_XML_DICOM_SR_FILE(_session, dicom_sr);
+				v_reports.add(xmldsr);
+			}			
+			return v_reports;
 		}
-
 	}
 
 }
